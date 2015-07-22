b67303cfecbc229aee033d23283c68341c37b2f2
Pain Admin <you@example.com>
2015-07-22 15:00:49 +0800

    Initial Commit

---
+++ XJCICP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XJCICP
+//
+//  Created by Will on 22/7/15.
+//  Copyright (c) 2015年 swin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XJCICP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XJCICP
+//
+//  Created by Will on 22/7/15.
+//  Copyright (c) 2015年 swin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
